67133eaca83959f57b49a0e310d377727a73acf7
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-09-27 18:54:32 -0500

    feat: set session-file-store

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import session from 'express-session'
+import FileStore from 'session-file-store'
 
 const app = express()
+const fileStore = new FileStore(session)
+
 app.use(session({
+    store: new fileStore({
+        path: './sessions'
+    }),
     secret: 'victoriasecret',
     resave: true,
     saveUninitialized: true
