c2c27fc7b050b0e55825cdc87f760db1cb60c52a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-09-27 19:06:00 -0500

    refactor: create router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import session from 'express-session'
 import FileStore from 'session-file-store'
+import userRouter from './routers/user.router.js'
 
 const app = express()
 const fileStore = new FileStore(session)
@@ -14,34 +15,6 @@ app.use(session({
     saveUninitialized: true
 }))
 
-const auth = (req, res, next) => {
-    if (req.session.user?.role === 'admin') {
-        next()
-    } else {
-        res.send('Not allowed!')
-    }
-}
-
-app.get('/', (req, res) => {
-    //despues de hacer la verificacion en la base de datos
-    //se identifica al user como 'alexmarinmendez' de rol 'user'
-    const user = {
-        username: 'alexmarinmendez',
-        role: 'admin'
-    }
-    req.session.user = user
-    res.send('ok')
-})
-
-app.get('/private', auth, (req, res) => {
-    res.send('Bienvenido!')
-})
-
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) return res.send('Logout error')
-        return res.send('Logout ok')
-    })
-})
+app.use('/', userRouter)
 
 app.listen(8080, () => console.log('Server Up'))
